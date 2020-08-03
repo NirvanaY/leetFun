87c9954678b8b227da657662f9f068cbca1d45ce
NirvanaY <765811944@qq.com>
2020-08-03 17:52:51 +0800

    never give up

---
+++ Demo/src/src/ListNode.java
@@ -0,0 +1,12 @@
+package src;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int i) {
+        this.val = i;
+    }
+}
+
+
--- Demo/src/src/OneByOne.java
+++ Demo/src/src/OneByOne.java
@@ -22,4 +22,33 @@ public class OneByOne {
     }
 
 
+
+//#2 add-two-numbers
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode first = new ListNode(0);
+        ListNode rs = first;
+        ListNode r1= l1;
+        ListNode r2 =l2;
+        int carry =0;
+
+        while (r1!=null || r2!=null) {
+            int x = (r1!=null)?r1.val:0;
+            int y = (r2!=null)?r2.val:0;
+            int sum = carry + x + y;
+            rs.next = new ListNode(sum%10);
+            rs = rs.next;
+            carry = sum/10;
+            if(r1!=null) r1= r1.next;
+            if(r2!=null) r2= r2.next;
+        }
+
+        if(carry>0){
+            rs.next = new ListNode(carry);
+        }
+
+        return first.next;
+    }
 }
+
+
